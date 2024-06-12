013bb99c48cd10c43a1299535067b0c73305b9c8
Mauro Machado <95579712+mau2602@users.noreply.github.com>
2024-06-12 15:30:43 +0200

    .js implementations

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,17 +1,43 @@
 const url = "./travel_recommendation_api.json"
+const beaches = ["beach", "beaches"] 
+const countries = ["country", "countries"]
+const temples = ["temple", "temples"]
 
-function getData(){
+
+function finding(userInput){
+    if(beaches.find(element => element == userInput)){
+        return "beaches"
+    } else if (countries.find(element => element == userInput)){
+        return "countries"
+    } else if(temples.find(element => element == userInput)){
+        return userInput
+    } else {
+        alert("Not found. Please try a different search.")
+    }
+}
+
+document.getElementById('searchBtn').addEventListener('click', (event) => {
+    event.preventDefault()
+    
+    let userInput = document.getElementById('searchInput').value
+    userInput = userInput.toLowerCase()
+    let search = finding(userInput)
+    if(search != undefined){
+            getData(search)
+    }
+})
+
+function getData(element){
     fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json(); 
+            .then(response => response.json())
+            .then(data => {
+                let container = document.getElementById('container')
+                let dataHTML = data[element].forEach(element => `<h1>Visit ${element.name}!</h1>
+                                                                 <p>${element.description}</p>`
+            )
+            container.innerHTML = dataHTML
         })
-        .then(data => {
-            console.log(data); 
-        })
-        .catch(error => {
-            console.error('Error fetching data: ', error);
-        });
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            });
 }
